bdecfaaebce708856a2f9898d34b2ce3285be06c
Ivan Saji Abraham <ivansaji619@gmail.com>
2020-12-15 07:20:27 +0530

    Updated Comments

--- hari_final_project/hari_classifier.py
+++ hari_final_project/hari_classifier.py
@@ -1,6 +1,28 @@
+#Testing Operating System Descriptions
+
+#OS : LMDE 4 Debbie
+#Version: 4.6.7
+#Kernal Version : 4.19.0-8-amd64
+
+#Scripting Langages : Python3
+
+#----------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#Libraries used
+
+#pandas is a fast, powerful, flexible and easy to use open source data analysis and manipulation tool,
 import pandas as pd
+
+#NumPy is a Python library used for working with arrays.
+#NumPy aims to provide an array object that is up to 50x faster than traditional Python lists.Arrays are very frequently used in data science, where speed and resources are very important.
 import numpy as np
+
+#Matplotlib is a comprehensive library for creating static, animated, and interactive visualizations in Python
+#matplotlib.pyplot is a state-based interface to matplotlib. It provides a MATLAB-like way of plotting.
+#pyplot is mainly intended for interactive plots and simple cases of programmatic plot generation
 import matplotlib.pyplot as plt
+
+#Simple and efficient tools for predictive data analysis. Built on NumPy, SciPy, and matplotlib. Scikit-learn is probably the most useful library for machine learning in Python. 
+#The sklearn library contains a lot of efficient tools for machine learning and statistical modeling including classification, regression, clustering and dimensionality reduction.
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn import metrics
@@ -8,6 +30,7 @@ from sklearn import metrics
 #-----------------------------------------------------------------------------------------------------------------------------
 #Reading the dataset
 dataset = pd.read_csv('./dataset/datafile.csv')
+
 #----------------------------------------------------------------------------------------------------------------------------
 #Extracting X and Y values
 #Preparing the Data
@@ -51,3 +74,4 @@ print('Root Mean Squared Error:', np.sqrt(metrics.mean_squared_error(y_test, y_p
 plt.scatter(X_test, y_test, color ='b') 
 plt.plot(X_test, y_pred, color ='k') 
 plt.show()
+
